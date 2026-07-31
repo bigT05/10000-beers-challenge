bf468691038c75697d38e965f4676d538a9f1aa6
bigT05 <thomascooper05@outlook.com>
2026-07-31 22:26:41 +0700

    Hiding menus

--- app.py
+++ app.py
@@ -76,34 +76,8 @@ st.markdown("""
         display: none !important;
     }
     
-    div[data-testid="stToolbar"] {
-    visibility: hidden;
-    height: 0%;
-    position: fixed;
+
     }
-    div[data-testid="stDecoration"] {
-    visibility: hidden;
-    height: 0%;
-    position: fixed;
-    }
-    div[data-testid="stStatusWidget"] {
-    visibility: hidden;
-    height: 0%;
-    position: fixed;
-    }
-    #MainMenu {
-    visibility: hidden;
-    height: 0%;
-    }
-    header {
-    visibility: hidden;
-    height: 0%;
-    }
-    footer {
-    visibility: hidden;
-    height: 0%;
-    }
-    
     </style>
 """, unsafe_allow_html=True)
 # ----------------------------------------------------------------------
