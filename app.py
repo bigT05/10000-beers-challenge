2db3f328289fd4160bc4a380736ae92bc141bd34
bigT05 <thomascooper05@outlook.com>
2026-07-31 22:24:10 +0700

    Hiding menus

--- app.py
+++ app.py
@@ -74,12 +74,36 @@ st.markdown("""
     /* Just in case Streamlit is using an SVG icon wrapper */
     .stMarkdown a svg {
         display: none !important;
-        }
-    
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
     }
+    
+    div[data-testid="stToolbar"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stDecoration"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stStatusWidget"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    #MainMenu {
+    visibility: hidden;
+    height: 0%;
+    }
+    header {
+    visibility: hidden;
+    height: 0%;
+    }
+    footer {
+    visibility: hidden;
+    height: 0%;
+    }
+    
     </style>
 """, unsafe_allow_html=True)
 # ----------------------------------------------------------------------
